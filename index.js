efbf0270e89cc70a1a59e13e4f91f0fa0378bb27
Lucas <lucasrocini@hotmail.com>
2022-08-30 21:24:27 -0300

    Implemented GET, Get by Id, POST, DELETE

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 import express from "express";
+import accountsRouter from "./routes/accounts.js";
+import {promises as fs} from "fs";
+
+const {readFile, writeFile} = fs;
 
 const app = express();
 app.use(express.json());
 
-app.listen(3000, () => {
-    
+app.use("/account", accountsRouter);
+
+app.listen(3000, async () => {
+    try{
+        await readFile("accounts.json");
+        console.log("API Started!")
+    }catch(err) {
+        const initialJson = {
+            nextId: 1,
+            accounts:[]
+        }
+        writeFile("accounts.json", JSON.stringify(initialJson)).then( () => {
+            console.log("API Started and file Created!");
+        }).catch(err => {
+            console.log(err);
+        })
+    }
 });
